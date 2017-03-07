5ad27752db7b243d3ecd5863e05b2f67efb09567
Karanpal Singh Dewgun <karan.devgan@hotmail.com>
2017-03-08 02:41:30 +0530

    Added code for selection using keyboard

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,52 @@
 function MapViewModel() {
     var self = this;
+
+    // Declare all the private variables here
+
     var timeout;
+    var keyCode = {
+        ARROW_DOWN: 40,
+        ARROW_UP: 38,
+        ENTER: 13
+    };
+    var currentIndex = -1;
+    var prevIndex = -1;
+    var searchItemsParent = document.getElementsByClassName('search-results');
+    var children = searchItemsParent[0].children;
+
+    // Declare all the objects here
+
     self.mapObj = new MapClass();
     self.init = mapObj.init(document.getElementById('map'));
     self.searchText = ko.observable("");
     self.addresses = ko.observable([]);
-    self.getAddresses = function () {
+
+    // Declare all the functions here
+
+    self.handleKeyPress = handleKeyPress;
+    self.resetSelection = resetSelection;
+    self.getAddresses = getAddresses;
+    self.setLocation = setLocation;
+
+    // Define all the functions here
+
+    function handleKeyPress(data, event) {
+        if (event.keyCode != keyCode.ARROW_DOWN && event.keyCode != keyCode.ARROW_UP && event.keyCode != keyCode.ENTER) {
+            self.addresses([]);
+            self.resetSelection();
+            self.getAddresses();
+        }
+        else {
+            selectChoice(event.keyCode);
+        }
+    }
+    
+    function resetSelection() {
+        currentIndex = -1;
+        prevIndex = -1
+    }
+
+    function getAddresses() {
         if (timeout) {
             clearInterval(timeout);
         }
@@ -23,8 +64,9 @@ function MapViewModel() {
         else {
             self.addresses([]);
         }
-    };
-    self.setLocation = function (address) {
+    }
+
+    function setLocation(address) {
         try {
             self.searchText(address.formatted_address);
             self.addresses([]);
@@ -36,6 +78,36 @@ function MapViewModel() {
         }
         catch (err) {
             console.log(err);
+        }
+    }
+
+    function selectChoice() {
+        if (event.keyCode === keyCode.ARROW_DOWN) {
+            if (children.length > 0) {
+                if (prevIndex != -1) {
+                    children[prevIndex].classList.remove('search-item-hover');
+                }
+                currentIndex = prevIndex + 1;
+                if (currentIndex > self.addresses().length - 1)
+                    currentIndex = 0;
+                children[currentIndex].classList.add('search-item-hover');
+                prevIndex = currentIndex;
+            }
+        } else if (event.keyCode === keyCode.ARROW_UP) {
+            if (children.length > 0) {
+                if (prevIndex != -1) {
+                    children[prevIndex].classList.remove('search-item-hover');
+                }
+                currentIndex = prevIndex - 1;
+                if (currentIndex < 0)
+                    currentIndex = self.addresses().length - 1;
+                children[currentIndex].classList.add('search-item-hover');
+                prevIndex = currentIndex;
+            }
+        } else if (event.keyCode === keyCode.ENTER) {
+            if (prevIndex != -1) {
+                children[prevIndex].click();
+            }
         }
     }
 }
